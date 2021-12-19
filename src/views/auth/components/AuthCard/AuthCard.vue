77777395f4f5751f397a4de13c66d4181599c44a
Javlon Khalimjonov <javlon.khalimjonov@movecloser.pl>
2021-12-19 15:13:28 +0100

    Add loader to login button in `AuthCard`

--- src/views/auth/components/AuthCard/AuthCard.vue
+++ src/views/auth/components/AuthCard/AuthCard.vue
@@ -2,9 +2,11 @@
   <div class="authorization__card">
     <h1>Socratus</h1>
     <STextInput placeholder="Email" size="normal" v-model="payload.phone"/>
+
     <STextInput placeholder="Password" type="password" size="normal" v-model="payload.password"/>
     <SDropdown :list="schoolsList" :value="selectedSchool" type="simple" @on-select="selectSchool"/>
-    <SButton label="Sign In" flat theme="secondary" @onClick="signIn"/>
+
+    <SButton label="Sign In" flat theme="secondary" @onClick="signIn" :isLoading="isLoading"/>
   </div>
 </template>
 
@@ -47,6 +49,8 @@ export class AuthCard extends Vue {
 
   public selectedSchool = 'Select school'
 
+  public isLoading = false
+
   public payload : { phone: string; password: string, tenant: string } = {
     phone: '+998903001105',
     password: 'Hello0909',
@@ -75,7 +79,9 @@ export class AuthCard extends Vue {
       return
     }
     this.errorMessage = null
+    this.isLoading = true
     await this.signInWith(this.payload)
+    this.isLoading = false
     await this.init()
   }
 }
